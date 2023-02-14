61c9328d1b8bcee5097f833404ccc68a07f0c6ed
Luis Felipe Zaguini <26530524+zaguiini@users.noreply.github.com>
2023-02-14 19:42:35 -0300

    Create zaguini.php

---
+++ plugins/zaguini.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Hello World
+ *
+ * @package     HelloWorld
+ * @author      Mehdi Nazari
+ * @copyright   2019 Mehdi Nazari
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Hello World
+ * Plugin URI:  https://mehdinazari.com/how-to-create-hello-world-plugin-for-wordpress
+ * Description: This plugin prints "Hello World" inside an admin page.
+ * Version:     1.0.0
+ * Author:      Mehdi Nazari
+ * Author URI:  https://mehdinazari.com
+ * Text Domain: hello-world
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
